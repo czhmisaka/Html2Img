637c6368b01cc5353dd98f82ff66ac5fa2f9e09f
czhmisaka <czh2233445566@163.com>
2025-05-08 09:19:07 +0800

    czh
    Signed-off-by: czhmisaka <czh2233445566@163.com>

--- html-to-image/utils/puppeteer.js
+++ html-to-image/utils/puppeteer.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-05-07 22:02:29
  * @LastEditors: CZH
- * @LastEditTime: 2025-05-08 01:54:41
+ * @LastEditTime: 2025-05-08 04:16:36
  * @FilePath: /html生成图片/html-to-image/utils/puppeteer.js
  */
 const puppeteer = require('puppeteer');
@@ -23,8 +23,8 @@ async function captureHTML(html, options = {}) {
 
         const page = await browser.newPage();
         await page.setViewport({
-            width: options.width || 1200,
-            height: options.height || 800,
+            width: options.width || 1920,
+            height: 800, // 初始高度，加载后会调整
             deviceScaleFactor: options.scale || 1
         });
 
@@ -32,10 +32,28 @@ async function captureHTML(html, options = {}) {
             waitUntil: 'networkidle0'
         });
 
+        // 获取页面实际高度
+        const bodyHeight = await page.evaluate(() => {
+            return Math.max(
+                document.body.scrollHeight,
+                document.body.offsetHeight,
+                document.documentElement.clientHeight,
+                document.documentElement.scrollHeight,
+                document.documentElement.offsetHeight
+            );
+        });
+
+        // 调整viewport高度
+        await page.setViewport({
+            width: options.width || 1200,
+            height: bodyHeight,
+            deviceScaleFactor: options.scale || 1
+        });
+
         const screenshotOptions = {
             type: options.type || 'png',
             ...(options.type === 'jpeg' ? { quality: options.quality || 80 } : {}),
-            fullPage: options.fullPage || false
+            fullPage: options.fullPage !== undefined ? options.fullPage : true
         };
 
         return await page.screenshot(screenshotOptions);
